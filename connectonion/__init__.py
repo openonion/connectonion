fac52f3f9d3879d725a2b00b90848251fdb025e8
aaron <changxing.wu@gmx.com>
2025-11-25 12:21:32 +1100

    Bump version to 0.4.5

--- connectonion/__init__.py
+++ connectonion/__init__.py
@@ -1,6 +1,6 @@
 """ConnectOnion - A simple agent framework with behavior tracking."""
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 
 # Auto-load .env files for the entire framework
 from dotenv import load_dotenv
